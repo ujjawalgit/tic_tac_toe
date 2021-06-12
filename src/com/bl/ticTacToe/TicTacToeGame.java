14c0051f056ab8abd28b6305e240e0576212fd1b
ujjawalgit <57063174+ujjawalgit@users.noreply.github.com>
2021-06-12 09:56:55 +0530

    added: creating a tic tac toe board

--- src/com/bl/ticTacToe/TicTacToeGame.java
+++ src/com/bl/ticTacToe/TicTacToeGame.java
@@ -1,7 +1,19 @@
 package com.bl.ticTacToe;
 
 public class TicTacToeGame {
+    public static char[] board = new char[10];
+
+    /*creating board  with empty spaces*/
+    public static char[] createBoard() {
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
+        return board;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToeGame");
+        /*createBoard method will create board with empty spaces*/
+        board = createBoard();
     }
 }
